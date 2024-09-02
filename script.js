15d55c9441c0b3b236a38adc1f50089bfffbc235
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-09-02 16:47:15 +0600

    added const and arrow function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('hello world');
+var country = "Bangladesh";
+console.log(country);
+var sum = function (num1, num2) {
+    console.log(num1 + num2);
+};
+sum(2, 3);
--- script.ts
+++ script.ts
@@ -1,1 +1,9 @@
 console.log('hello world')
+
+const country = "Bangladesh";
+console.log(country);
+
+const sum = (num1 , num2) =>{
+    console.log(num1 + num2)
+}
+sum(2 , 3);
