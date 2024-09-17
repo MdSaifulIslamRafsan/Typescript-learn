bc864a28490f8a04a6421469f53ccdafa39c4a48
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-09-17 18:08:43 +0600

    added type define

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+"use strict";
 console.log('hello world');
-var country = "Bangladesh";
+const country = "Bangladesh";
 console.log(country);
-var sum = function (num1, num2) {
+const sum = (num1, num2) => {
     console.log(num1 + num2);
 };
 sum(2, 3);
--- script.ts
+++ script.ts
@@ -3,7 +3,7 @@ console.log('hello world')
 const country = "Bangladesh";
 console.log(country);
 
-const sum = (num1 , num2) =>{
+const sum = (num1: number , num2: number) =>{
     console.log(num1 + num2)
 }
 sum(2 , 3);
